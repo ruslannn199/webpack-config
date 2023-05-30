6b4581b7d0358232c6fa03fca0f1f7b641e8dad6
ruslannn199 <ruslannn199@gmail.com>
2023-05-30 18:14:58 +0300

    feat: css and babel update

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,47 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+const target = isDev ? 'web' : 'browserslist';
 
 module.exports = {
-  mode: "development",
+  mode: mode,
+  target: target,
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 
-  devtool: false,
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+
+  devtool: isDev ? 'source-map' : 'hidden-nosources-source-map',
   devServer: {
-    contentBase: "./dist",
+    port: 9000,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    open: true,
+    hot: isDev,
   },
 }
