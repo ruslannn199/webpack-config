5ae36a10d73ee73cb0cd518c96da663d48593877
ruslannn199 <ruslannn199@gmail.com>
2023-05-30 18:53:58 +0300

    feat: update resources

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,47 @@ const isDev = process.env.NODE_ENV === 'development';
 const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
 const target = isDev ? 'web' : 'browserslist';
 
+const filename = (ext) => (isDev ? `[name].${ext}` : `[contenthash].${ext}`);
+
 module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: 'images/[contenthash][ext]',
+  },
+
   module: {
     rules: [
       {
-        test: /\.s?css$/i,
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.(wav|ogg|mp3)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/sound/[contenthash][ext]',
+        }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' },
+          },
           'css-loader',
           'postcss-loader',
           'sass-loader'
